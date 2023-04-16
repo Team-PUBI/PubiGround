5dd143a2a4bec30b6158da23d0476fd10cf86b0e
Rio-323 <kimsiyeon5214@gmail.com>
2023-04-16 12:22:39 +0900

    Initial commit

---
+++ src/main/java/com/pubi/PubiGround/PubiGroundApplication.java
@@ -0,0 +1,13 @@
+package com.pubi.PubiGround;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubiGroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubiGroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pubi/PubiGround/PubiGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pubi.PubiGround;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubiGroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
